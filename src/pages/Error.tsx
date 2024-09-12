7247e48001a8ed32ffb09c413bf7fb8ec0816137
sikim07 <skim88.1942@gmail.com>
2024-09-12 14:53:24 +0900

    Feat: add error page

---
+++ src/pages/Error.tsx
@@ -0,0 +1,13 @@
+const Error = (): JSX.Element => {
+	return (
+		<>
+			<h2>Oops!</h2>
+			<p className="pt-[20px]">Sorry, an unexcepted error has occured.</p>
+			<p className="pt-[10px]">
+				<i className="pt">Not Found</i>
+			</p>
+		</>
+	)
+}
+
+export default Error
