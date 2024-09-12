5025e0c12f8456f2800288ff045215097edec14e
sikim07 <skim88.1942@gmail.com>
2024-09-12 14:48:22 +0900

    Feat: TailwindCSS에 반응형 사이즈 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
 	content: ["./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				xl: { min: "1680px" },
+				lg: { min: "1024px", max: "1679px" },
+				md: { min: "768px", max: "1023px" },
+				sm: { min: "360px", max: "767px" },
+				xs: { max: "359px" },
+				xm: { max: "767px" },
+				xd: { max: "1023px" },
+				xg: { max: "1679px" },
+				ml: { min: "1024px" },
+			},
+		},
 	},
 	plugins: [],
 }
