ac69ec6cf5ddfaa09d66bd3752bbcd8940c6fe24
sikim07 <skim88.1942@gmail.com>
2024-09-13 15:04:09 +0900

    WIP: blog list page

--- src/components/blog/BlogList.tsx
+++ src/components/blog/BlogList.tsx
@@ -1,24 +1,37 @@
 import { ReactNode } from "react"
+import { Link } from "react-router-dom"
 import { Blog } from "@/types/blog"
 
 const BlogList = ({ children }: ReactNode): JSX.Element => {
-	return <section>{children}</section>
+	return (
+		<section className="w-[940px] xd:w-full xd:px-[24px] pt-[50px] flex flex-col gap-[20px]">
+			{children}
+		</section>
+	)
 }
 
 const BlogItem = (item: Blog): JSX.Element => {
 	return (
-		<article>
-			<div>
-				<h2>{item.title}</h2>
-				<p>{item.content}</p>
-				<p>{item.date}</p>
+		<article className="flex justify-between pb-[20px] border-b border-solid border-[#d3d3d3]">
+			<div className="max-w-[80%]">
+				<Link to={`/blog/${item.id}`}>
+					<h2 className="tracking-[-0.04em] text-[24px] font-bold line-clamp-3">
+						{item.title}
+					</h2>
+					<p className="tracking-[-0.04em] text-[16px] text-[#d3d3d3] pt-[8px] line-clamp-1">
+						{item.content}
+					</p>
+					<p className="leading-[58px] tracking-[-0.04em] text-[16px] text-[#d3d3d3]">
+						{item.date}
+					</p>
+				</Link>
 			</div>
-			<div>
+			<div className="">
 				{item.img && (
-					<>
+					<Link to={`/blog/${item.id}`}>
 						<div>{item.img}</div>
 						<div>{item.imgAlt ?? ""}</div>
-					</>
+					</Link>
 				)}
 			</div>
 		</article>
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,7 +1,7 @@
 const Layout = ({ children }) => {
 	return (
-		<div className="mx-auto w-[944px] xd:w-full p-[20px_0px_40px] xd:p-[20px_24px_40px]">
-			<header className="h-[64px] items-center flex justify-between">
+		<div className="mx-auto w-[944px] xd:w-full p-[0px_0px_40px] xd:p-[0px_24px_40px]">
+			<header className="h-[64px] items-center flex justify-between border-b border-solid border-[#d3d3d3]">
 				<button className="bg-transparent font-serif font-bold text-[24px] italic p-0">
 					SIKIM88
 				</button>
